59eb9efcd59138807852366ed0e8faeb50daa2d4
Giancarlo Sanchez <gianksp@gmail.com>
2022-10-02 20:47:33 +0100

    New plugin smart contract ui

--- src/lib/Editor/index.js
+++ src/lib/Editor/index.js
@@ -3,6 +3,7 @@ import grapesjs from "grapesjs";
 import PluginWallet from "dappify-wallet-module";
 import PluginTailwind from "dappify-tailwind-module";
 import PluginEditorPanelButtons from "./Panel/Buttons";
+import PluginSmartContract from "dappify-smart-contract-ui-module";
 import * as LandingPage from "../templates/LandingPage";
 import ConfirmationModal from "../views/modal/Confirmation";
 import { DappifyContext } from "react-dappify";
@@ -40,8 +41,11 @@ const Editor = ({ projectId, onClickHome }) => {
                 <html lang="en">
                   <head>
                     <meta charset="UTF-8">
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
                     <script src="https://cdn.tailwindcss.com"></script>
+                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
                     <script src="https://cdn.ethers.io/lib/ethers-5.0.umd.min.js"></script>
                     <script src="https://unpkg.com/web3modal@1.9.0/dist/index.js"></script>
                     <script src="https://unpkg.com/evm-chains@0.2.0/dist/umd/index.min.js"></script>
@@ -52,7 +56,6 @@ const Editor = ({ projectId, onClickHome }) => {
                   ${html}
                 </html>`;
 
-      // Upload to IPFS
       var blob = new Blob([content], { type: "text/plain" });
       const file = new Provider.File("project", blob);
       const deployed = await file.saveIPFS();
@@ -101,19 +104,27 @@ const Editor = ({ projectId, onClickHome }) => {
       fromElement: true,
       storageManager: false,
       selectorManager: { escapeName },
-      plugins: [PluginTailwind, PluginEditorPanelButtons, PluginWallet],
+      plugins: [
+        PluginTailwind,
+        PluginEditorPanelButtons,
+        PluginWallet,
+        PluginSmartContract,
+      ],
       pluginsOpts: {},
       canvas: {
         scripts: [
+          "https://code.jquery.com/jquery-3.6.1.min.js",
+          "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js",
           "https://cdn.ethers.io/lib/ethers-5.0.umd.min.js",
           "https://unpkg.com/web3modal@1.9.0/dist/index.js",
           "https://unpkg.com/evm-chains@0.2.0/dist/umd/index.min.js",
           "https://unpkg.com/@walletconnect/web3-provider@1.3.1/dist/umd/index.min.js",
           "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.js",
+          "https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js",
         ],
         // The same would be for external styles
         styles: [
-          "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css",
+          "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
         ],
       },
     });
